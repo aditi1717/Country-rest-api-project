a09a53b58954e8cab84d93695d8e8a54346f7da7
aditi <aditiparihar179@gmail.com>
2025-07-22 15:33:54 +0530

    add dark and  light mode one way

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react"
 
 
 export default function Header() {
+
+  const [isDark,setIsDark]=useState(JSON.parse(localStorage.getItem("isDarkMode")));
+  if(isDark){
+      document.body.classList.add('dark')
+  }
+  else{
+     document.body.classList.remove('dark')
+  }
+  // console.log(JSON.parse(localStorage.getItem("isDarkMode")));
+ 
+  
   return (
     <header className="header">
         <h2 ><a href="/" className="title">Where in the world?</a></h2>
-        <p className="dark-mode"> <i className="fa-regular fa-moon"></i><span className="dark-mode">Dark Mode</span></p>
+        <p className="dark-mode" onClick={()=>{
+          setIsDark(!isDark);
+          localStorage.setItem("isDarkMode",!isDark)
+          
+        }}> <i className={`fa-solid fa-${isDark?"sun":"moon"}`}></i><span className="dark-mode">{isDark?"Light ":"Dark "}Mode</span></p>
     </header>
   )
 }
